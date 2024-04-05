2325a0c8fcd8c61e787822028bb2a88e10a8070a
Sobackin <164174146+Sobackin@users.noreply.github.com>
2024-04-05 23:08:50 +0900

    Add files via upload
    계산기 기본원리 기초 시작!

---
+++ app.js
@@ -0,0 +1,23 @@
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  devide: function (a, b) {
+    console.log(a / b);
+  },
+  times:function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.add(1, 2);
+calculator.minus(1, 2);
+calculator.devide(1, 2);
+calculator.times(1, 2);
+calculator.power(1, 2);
