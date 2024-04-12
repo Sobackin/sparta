2661d9fe48c452e1e6ea5a3c4ae4d717b3b80a4d
Sobackin <164174146+Sobackin@users.noreply.github.com>
2024-04-12 14:56:26 +0900

    EventClick

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  devide: function (a, b) {
-    console.log(a / b);
-  },
-  times:function (a, b) {
-    console.log(a * b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const title = document.querySelector("div.hello:first-child h1");
 
-calculator.add(1, 2);
-calculator.minus(1, 2);
-calculator.devide(1, 2);
-calculator.times(1, 2);
-calculator.power(1, 2);
+function handleTitleClick(){
+    console.log("title was clicked!")
+}
+
+title.addEventListener("click", handleTitleClick);
